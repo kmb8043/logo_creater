2e525e724d5d400cf546ca5b0c36c810cb4b5cfa
kmb8043 <you@example.com>
2024-01-24 13:09:01 -0500

    made a lot of changes. edited the shapes.js still cant get the photo of the logo to show

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ This project utilizes the terminal to creat a logo.
 
 The user will open the working folder terminal. Run test and will then receive prompts asking the user questions in order to create a logo. The generated logo will be saved into a folder for the user to use later in a project. 
 
+
 ## GitHub ##
 
 kmb8043
--- index.js
+++ index.js
@@ -1,49 +1,87 @@
 // packages importing //
-const fs = require('fs');
+const fs = require('fs/promises');
 const inquirer = require('inquirer');
 const { triangle, square, circle } = require('./lib/shapes');
 
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                message: 'Enter three characters to include in your logo',
-                name:'text',
-            },
 
-            {
-                type:'input',
-                message: 'Pick text color for the text',
-                name: 'textColor',
-            },
+const questions = [  // this is our dataset (questionset)
+    {
+        type: 'input',
+        message: 'Enter three characters to include in your logo',
+        name:'text',
+    },
 
-            {
-                type: 'list',
-                message: 'Pick a shape',
-                name: 'shape',
-                choices: ['triangle' , 'square' , 'cirlce'],
-            },
+    {
+        type:'input',
+        message: 'Pick text color for the text',
+        name: 'textColor',
+        
+    },
 
-            {
-                type: 'input',
-                message: 'Choose a background color for your shape',
-                name: 'shapeColor',
+    {
+        type: 'list',
+        message: 'Pick a shape',
+        name: 'shape',
+        choices: ['triangle' , 'square' , 'circle'],
+    },
+
+    {
+        type: 'input',
+        message: 'Choose a background color for your shape',
+        name: 'shapeColor',
+    }
+]
+
+/*
+inquirer
+    .then(function(answers) {  // Here we HANDLE the PROMISE
+        console.log("Data: ", answers);
+        // what do we do next?
+    })
+    .catch(error => {
+        console.log("An error occured: ", error);
+        // we could also exit out of our program
+    });
+*/
+        function createLogo(answers){
+            if(answers.shape ==='triangle'){
+                //let logo = new triangle(answers.text, answers.textColor, answers.shapeColor)
+                let logo = new triangle(answers.shapeColor, answers.text, answers.textColor)
+                return logo.render()
+            } else if (answers.shape === 'square'){
+                //let logo = new square (answers.text, answers.textColor, answers.shapeColor)
+                let logo = new square (answers.shapeColor, answers.text, answers.textColor)
+                return logo.render()
+            } else {
+               // let logo = new circle (answers.text, answers.textColor, answers.shapeColor)
+                let logo = new circle (answers.shapeColor, answers.text, answers.textColor)
+                return logo.render()
             }
-        ])
+            
+        }
+        
 
-        .then((data) => {
-            const { text, textColor, shape, shapeColor} = data;
-            var svglogo;
-            switch (shape){
-                case 'triangle':
-                    svglogo = new triangle(text, textColor, shapeColor);
-                    break;
-                case 'square':
-                    svglogo = new square(text, textColor, shapeColor);
-                    break;
-                case 'circle':
-                    svglogo = new circle(text, textColor, shapeColor);
-            }
-// created file goes into the examples folder //
-    fs.writeFileSync('./examples/logo.svg' , svglogo.render());
-        });
+
+        function saveLogo(answers){
+            let customizedLogo = createLogo(answers);
+            fs.writeFile('./examples/logo.svg', customizedLogo);
+        }
+        
+        function init (){
+            inquirer
+                .prompt(questions)
+                .then((answers) => {
+                    console.log("Data: ", answers)
+                    saveLogo(answers);
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
+
+                // what might or could happen while this operation is running(?)
+        }
+
+        init();
+
+        
